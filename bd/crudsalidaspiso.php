2208af101637d9f1d80ff0d09b7d78e699ffa4bc
SwaggerSly <axelutc62@gmail.com>
2021-08-11 17:20:08 -0500

    Crud 100% hecho

---
+++ bd/crudsalidaspiso.php
@@ -0,0 +1,35 @@
+<?php
+include_once '../bd/conexion.php';
+$objeto = new Conexion();
+$conexion = $objeto->Conectar();
+
+$id_prod = (isset($_POST['id_prod'])) ? $_POST['id_prod'] : '';
+$cantidad = (isset($_POST['cantidad'])) ? $_POST['cantidad'] : '';
+$salida = (isset($_POST['salida'])) ? $_POST['salida'] : '';
+$user = (isset($_POST['user'])) ? $_POST['user'] : '';
+
+$opcion = (isset($_POST['opcion'])) ? $_POST['opcion'] : '';
+$id_salida = (isset($_POST['id_salida'])) ? $_POST['id_salida'] : '';
+
+
+switch($opcion){
+    case 1:
+        $consulta = "INSERT INTO salidas (id_prod, cantidad, salida, user) VALUES('$id_prod', '$cantidad', '$salida', '4') ";			
+        $resultado = $conexion->prepare($consulta);
+        $resultado->execute(); 
+        
+        $consulta = "SELECT * FROM salidas ORDER BY id_salida DESC LIMIT 1";
+        $resultado = $conexion->prepare($consulta);
+        $resultado->execute();
+        $data=$resultado->fetchAll(PDO::FETCH_ASSOC);       
+        break;    
+    case 2:    
+        $consulta = "SELECT * FROM salidas";
+        $resultado = $conexion->prepare($consulta);
+        $resultado->execute();        
+        $data=$resultado->fetchAll(PDO::FETCH_ASSOC);
+        break;
+}
+
+print json_encode($data, JSON_UNESCAPED_UNICODE);//envio el array final el formato json a AJAX
+$conexion=null;
---
+++ mainSalidaspiso.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+    var id_salida, opcion;
+    opcion = 2;
+        
+    tablaSalidas = $('#tablaSalidas').DataTable({  
+        "ajax":{            
+            "url": "bd/crudsalidaspiso.php", 
+            "method": 'POST', //usamos el metodo POST
+            "data":{opcion:opcion}, //enviamos opcion 4 para que haga un SELECT
+            "dataSrc":""
+        },
+        "columns":[
+            {"data": "id_salida"},
+            {"data": "id_prod"},
+            {"data": "cantidad"},
+            {"data": "salida"},
+            {"data": "user"},
+        ]
+    });     
+    
+    var fila; //captura la fila, para editar o eliminar
+    //submit para el Alta y Actualización
+    $('#formSalidas').submit(function(e){                         
+        e.preventDefault(); //evita el comportambiento normal del submit, es decir, recarga total de la página
+        id_prod = $.trim($('#id_prod').val());    
+        cantidad = $.trim($('#cantidad').val());
+        salida = $.trim($('#salida').val());    
+        user = $.trim($('#user').val());                                
+            $.ajax({
+              url: "bd/crudsalidaspiso.php",
+              type: "POST",
+              datatype:"json",    
+              data:  {id_salida:id_salida, id_prod:id_prod, cantidad:cantidad, salida:salida, user:user,opcion:opcion},    
+              success: function(data) {
+                tablaSalidas.ajax.reload(null, false);
+               }
+            });			        
+        $('#modalCRUD').modal('hide');											     			
+    });
+            
+     
+    
+    //para limpiar los campos antes de dar de Alta una Persona
+    $("#btnNuevo").click(function(){
+        opcion = 1; //alta           
+        id_salida=null;
+        $("#formSalidas").trigger("reset");
+        $(".modal-header").css( "background-color", "#17a2b8");
+        $(".modal-header").css( "color", "white" );
+        $(".modal-title").text("Registrar Salida");
+        $('#modalCRUD').modal('show');	    
+    });
+         
+    });    
